ccb07231b0950d842008cc4d9067041e4b82b1e8
RoDFS-GH <rosana.19abril@gmail.com>
2023-05-27 04:38:00 -0300

    Primer Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ListsPosts from "../components/ListPosts"
+import ListCategories from "../components/ListCategories"
+
+const Home = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Pet noticias</h2>
+      </div>
+      <ListCategories />
+      <ListsPosts url={"/posts"}/>
+    </main>
+  )
+}
+
+export default Home
